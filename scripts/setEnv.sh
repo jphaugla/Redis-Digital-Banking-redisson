82ffc68bc7fc4514c446ec4413ae11d33a4fd569
Jason Haugland <jasonhaugland@gmail.com>
2022-01-14 13:17:09 -0600

    add password for redisson

--- scripts/setEnv.sh
+++ scripts/setEnv.sh
@@ -3,3 +3,4 @@ export REDIS_REPLICA1="redis://localhost:6380"
 export REDIS_REPLICA2="redis://localhost:6381"
 export CORE_POOLSIZE=20
 export READ_MODE="MASTER_SLAVE"
+export REDIS_PASSWORD=ps2kpXjk
